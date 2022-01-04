e4971d4141d38ab7cf6f165fa80124b463c8a037
Alana Correia <alanacorreia.ac@gmail.com>
2022-01-04 20:19:09 -0300

    aplicacao do css no projeto

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,13 +1,14 @@
 import { React, Component } from 'react';
 import PropTypes from 'prop-types';
+import '../PlanetCard.css';
 
 class PlanetCard extends Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
       <div data-testid="planet-card">
+        <img className="Img" src={ planetImage } alt={ `Planeta ${planetName}` } />
         <h3 data-testid="planet-name">{planetName}</h3>
-        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
     );
   }
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -2,11 +2,12 @@ import { React, Component } from 'react';
 import Title from './Title';
 import planets from '../data/planets';
 import PlanetCard from './PlanetCard';
+import '../App.css';
 
 class SolarSystem extends Component {
   render() {
     return (
-      <div data-testid="solar-system">
+      <div data-testid="solar-system" className="System">
         <Title headline="Planetas" />
         {planets.map((planet) => (
           <PlanetCard
